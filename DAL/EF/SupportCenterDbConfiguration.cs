5473670ad388020244c7a88ea1d23ae53c522b86
Jasper Vancoillie <jasper.vancoillie@student.kdg.be>
2018-02-28 15:57:48 +0100

    ctr-z

--- DAL/EF/SupportCenterDbConfiguration.cs
+++ DAL/EF/SupportCenterDbConfiguration.cs
@@ -7,15 +7,13 @@ using System.Threading.Tasks;
 
 namespace SC.DAL.EF
 {
-    internal class SupportCenterDbConfiguration : DbConfiguration
+  internal class SupportCenterDbConfiguration : DbConfiguration
+  {
+    public SupportCenterDbConfiguration()
     {
-        public SupportCenterDbConfiguration()
-        {
-            this.SetDefaultConnectionFactory(
-                new System.Data.Entity.Infrastructure.SqlConnectionFactory());
-
-            this.SetProviderServices("System.Data.SqlClient",
-                System.Data.Entity.SqlServer.SqlProviderServices.Instance);
-        }
+      this.SetDefaultConnectionFactory(new System.Data.Entity.Infrastructure.SqlConnectionFactory()); // SQL Server instantie op machine
+      
+      this.SetProviderServices("System.Data.SqlClient", System.Data.Entity.SqlServer.SqlProviderServices.Instance);
     }
+  }
 }
