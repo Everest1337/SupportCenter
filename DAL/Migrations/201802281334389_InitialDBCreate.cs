89886bf506a0287c4d7c8a6940a8e50871649dd4
Jasper Vancoillie <jasper.vancoillie@student.kdg.be>
2018-02-28 15:56:13 +0100

    Uploading project

---
+++ DAL/Migrations/201802281334389_InitialDBCreate.cs
@@ -0,0 +1,16 @@
+namespace SC.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDBCreate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
